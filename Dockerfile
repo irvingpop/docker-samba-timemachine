57daa8b28f13aa226c809c567483a47b8124afb9
Irving Popovetsky <irving@atlas.popovetsky.com>
2019-01-06 07:02:01 +0000

    Some cleanups and add a second share, for storing regular files.  also make sure samba var stuff also gets bind mounted, so it persists restarts of the container
    Signed-off-by: Irving Popovetsky <irving@atlas.popovetsky.com>

--- Dockerfile
+++ Dockerfile
@@ -10,14 +10,14 @@ RUN apk add --update \
     && sed -i 's/#enable-dbus=yes/enable-dbus=no/g' /etc/avahi/avahi-daemon.conf \
     && rm -rf /var/cache/apk/*
 
+RUN rm -rf /etc/avahi/services/*.service
 
 COPY setup.sh template_quota /tmp/
 COPY smb.conf /etc/samba/smb.conf
 COPY avahia.service /etc/avahi/services/timemachine.service
 COPY supervisord.conf /etc/supervisord.conf
-#RUN /tmp/setup.sh
 
-VOLUME ["/timemachine"]
+VOLUME ["/shares"]
 ENTRYPOINT ["/tmp/setup.sh"]
 HEALTHCHECK --interval=5m --timeout=3s \
   CMD (avahi-daemon -c && \
--- setup.sh
+++ setup.sh
@@ -1,24 +1,26 @@
-#!/bin/sh
+#!/bin/sh -e
 
 #defaults
 TM_USER="${TM_USER:-timemachine}"
 TM_PW="${TM_PW:-timemachine}"
 TM_ID="${TM_ID:-1000}"
-TM_SIZE="${TM_SIZE:-512000}"
+TM_SIZE="${TM_SIZE:-1024000}"
+
+## create necessary directories
+for dir in /shares/timemachine /shares/public /var/cache/samba /var/lib/samba/private /var/log/samba /var/run/samba; do
+  if [ ! -d "${dir}" ]; then
+    mkdir -p "${dir}"
+  fi
+done
+
 #Add smb user
-    grep -q "^${TM_USER}:" /etc/passwd ||
-        adduser -D -H ${TM_GROUP:+-G $TM_GROUP} ${TM_ID:+-u $TM_ID} "${TM_USER}"
-    echo -e "${TM_PW}\n${TM_PW}" | smbpasswd -s -a "${TM_USER}"
+grep -q "^${TM_USER}:" /etc/passwd || \
+  adduser -D -H ${TM_GROUP:+-G $TM_GROUP} ${TM_ID:+-u $TM_ID} "${TM_USER}"
+echo -e "${TM_PW}\n${TM_PW}" | smbpasswd -s -a "${TM_USER}"
 
-## timemachine location
-if [ ! -d "/timemachine" ]; then
-    mkdir "/timemachine"
-fi
-
-
-chown -R ${TM_USER} "/timemachine"
+chown -R ${TM_USER} "/shares"
 TM_SIZE=$(($TM_SIZE * 1000000))
-sed "s#REPLACE_TM_SIZE#${TM_SIZE}#" /tmp/template_quota > /timemachine/.com.apple.TimeMachine.quota.plist
+sed "s#REPLACE_TM_SIZE#${TM_SIZE}#" /tmp/template_quota > /shares/timemachine/.com.apple.TimeMachine.quota.plist
 
 # run CMD
 exec "${@}"
